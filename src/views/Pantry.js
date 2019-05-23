bd9fc932e03889f0b48dc2fdb22be9b88b2d0f7a
Scott Mallinson <scott@scottmallinson.com>
2019-05-23 21:08:08 +0200

    Added dynamic form to pantry

--- src/views/Pantry.js
+++ src/views/Pantry.js
@@ -1,20 +1,77 @@
 import React, { Component } from "react";
 import { withAuth } from "../lib/AuthProvider";
+const axios = require('axios');
 class Pantry extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pantry: this.props.user.pantry
+    }
+  } 
+
+  handleItemChange(e, inputIndex) {
+    const {pantry} = this.state;
+    let newPantry = [...pantry];
+    newPantry.map((_, index, newPantry)=>{
+      return index === inputIndex ? newPantry[index] = e.target.value : null;
+    })
+    this.setState({
+      pantry: newPantry,
+    })
+  }
+
+  handleItemRemove(e, index) {
+    e.preventDefault();
+    this.state.pantry.splice(index, 1);
+    this.setState({
+      pantry: this.state.pantry
+    })
+  }
+
+  addItem(e, items) {
+    e.preventDefault();
+    this.setState({
+      pantry: [...this.state.pantry, '']
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+    const { pantry } = this.state;
+    axios.put('http://localhost:5000/user/pantry', {
+      pantry
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+  }
+
   render() {
+    console.log(this.props.user.pantry);
     return (
       <div>
         <h2>Pantry.js</h2>
         <h1>Welcome {this.props.user.username}</h1>
         <p>This is your pantry</p>
-        {this.props.user.pantry.map((item) => {
+        {/* {this.state.pantry.map((item, index) => {
           return (
-            <div key={item._id}>
+            <div key={index}>
               <p><strong>{item.item}</strong> - {item.quantity}</p>
             </div>
           )
-        })}
-        <button>Add an item</button>
+        })} */}
+        {
+            this.state.pantry.map((item, index) => {
+              return (
+                <div key={index}>
+                  <input onChange={(e) => this.handleItemChange(e, index)} value={item.item} />
+                  <button onClick={(e) => this.handleItemRemove(e, index)}>Remove</button>
+                </div>
+              )
+            })
+          }
+          <button onClick={(e) => this.addItem(e)}>Add item</button>
+          <button onClick={(e) => this.handleSubmit(e)}>Save items</button> 
       </div>
     );
   }
