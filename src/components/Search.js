f11c8d17b025706e4939ffaccdd9d03619979d07
Scott Mallinson <scott@scottmallinson.com>
2019-05-25 23:14:31 +0200

    Added test function to post all pantry items to backend

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { withAuth } from "../lib/AuthProvider";
 const axios = require('axios');
 class Search extends Component {
   constructor(props) {
+    const pantryContents = (pantryItems) => { 
+      return pantryItems.map((item) => item.item )
+    } 
     super(props)
     this.state = {
       searchTerm: '',
+      pantry: pantryContents(this.props.pantry),
       recipes: []
     }
   }
@@ -20,12 +25,26 @@ class Search extends Component {
       .catch((error) => console.log(error));
   }
 
+  handleLucky = (e) => {
+    e.preventDefault();
+    const searchForItems = this.state.pantry;
+    axios.post('http://localhost:5000/recipes/search', {
+      searchForItems
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+  }
+
   render() {
     return (
       <div>
         <form>
           <input type="text" autoFocus name="searchTerm" value={this.state.searchTerm} placeholder="Find a recipe" onChange={(e) => this.handleSearch(e)} />
         </form>
+        {this.props.isLoggedin ?
+        <button onClick={(e) => this.handleLucky(e)}>I'm feeling lucky</button>
+        :
+        null }
         {this.state.recipes.map((recipe) => 
           <Link key={recipe._id} to={{
             pathname: `/recipes/${recipe._id}`,
@@ -40,4 +59,4 @@ class Search extends Component {
   }
 }
 
-export default Search;
+export default withAuth(Search);
