d47b46e324801840d25754fe3ac623cdaa3c8c7b
Scott Mallinson <scott@scottmallinson.com>
2019-05-26 11:48:30 +0200

    Removed pantry ingredients search from Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,26 +25,12 @@ class Search extends Component {
       .catch((error) => console.log(error));
   }
 
-  handleLucky = (e) => {
-    e.preventDefault();
-    const searchForItems = this.state.pantry;
-    axios.post('http://localhost:5000/recipes/search', {
-      searchForItems
-    })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
-  }
-
   render() {
     return (
       <div>
         <form>
           <input type="text" autoFocus name="searchTerm" value={this.state.searchTerm} placeholder="Find a recipe" onChange={(e) => this.handleSearch(e)} />
         </form>
-        {this.props.isLoggedin ?
-        <button onClick={(e) => this.handleLucky(e)}>I'm feeling lucky</button>
-        :
-        null }
         {this.state.recipes.map((recipe) => 
           <Link key={recipe._id} to={{
             pathname: `/recipes/${recipe._id}`,
