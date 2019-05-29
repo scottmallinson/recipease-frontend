17ab3c3df8f991554cef4ab47a9db0b4e9fc62f6
Scott Mallinson <scott@scottmallinson.com>
2019-05-29 21:47:09 +0200

    Style tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="navbar fixed-bottom navbar-light bg-light">
+      <footer className="navbar fixed-bottom navbar-light bg-light border-top">
         <div className="container p-0">
-          <span className="navbar-text small" href="/">&copy; Recipease - Scott Mallinson 2019.</span>
+          <span className="navbar-text small" href="/">&copy; 2019 Recipease &mdash; <a href="https://scottmallinson.com" target="_blank" rel="me noopener noreferrer" className="text-reset">Scott Mallinson</a></span>
         </div>
       </footer>
     );
