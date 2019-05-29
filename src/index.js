0bb0d03914b27887e52de1c96194ecd759e6cde1
Scott Mallinson <scott@scottmallinson.com>
2019-05-29 22:01:43 +0200

    Fix for scroll position being preserved across views

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import App from './App';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </Router>
   , document.getElementById('root'));
