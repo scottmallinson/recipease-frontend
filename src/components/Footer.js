ad4c2bb71dea6f31cbe61b6344ff829b470f3ed5
Scott Mallinson <scott@scottmallinson.com>
2019-06-04 15:02:05 +0200

    Removed fixed positioning of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="navbar fixed-bottom navbar-light bg-light border-top">
+      <footer className="navbar navbar-light bg-light border-top">
         <div className="container p-0">
           <span className="navbar-text small" href="/">&copy; 2019 Recipease &mdash; <a href="https://scottmallinson.com" target="_blank" rel="me noopener noreferrer" className="text-reset">Scott Mallinson</a></span>
         </div>
