76928ef23f2e8b438cebf018df68fb08bd8838a9
Scott Mallinson <scott@scottmallinson.com>
2019-06-10 18:53:21 +0200

    Tidied up interaction states. Refactored with React hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { withRouter } from 'react-router';
 import { Link } from "react-router-dom";
 import { withAuth } from "../lib/AuthProvider";
 import logo from './../assets/logo.svg';
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      location: null,
-      navCollapsed: true
-    }
+function Navbar(props) {
+  const [navCollapsed, setNavCollapsed] = useState(true);
+
+  function handleToggleNav() {
+    setNavCollapsed(!navCollapsed);
   }
 
-  handleToggleNav = () => {
-    this.setState({ navCollapsed: !this.state.navCollapsed })
+  function handleLogout() {
+    handleToggleNav()
+    props.logout()
   }
 
-  handleLogout = () => {
-    this.handleToggleNav()
-    this.props.logout()
-  }
-
-  render() {
-    const { navCollapsed } = this.state
-    const { isLoggedin } = this.props;
-    return (
-      <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top border-bottom">
-        <div className="container">
-          <Link className="navbar-brand" to="/" onClick={this.handleToggleNav}>
-            <img src={logo} width="30" height="30" className="d-inline-block align-top pr-1" alt="" />
-            Recipease
-        </Link>
-          <button data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" className='navbar-toggler collapsed' onClick={this.handleToggleNav} type="button">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className={(navCollapsed ? 'collapse' : '') + ' navbar-collapse'}>
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/" onClick={this.handleToggleNav}>Home <span className="sr-only">(current)</span></Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/recipes" onClick={this.handleToggleNav}>Recipes</Link>
-              </li>
-              {isLoggedin ? (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profile" onClick={this.handleToggleNav}>Profile</Link>
-                </li>
-              ) : (null)}
-            </ul>
-            
-            <ul className="navbar-nav">
+  const { isLoggedin } = props;
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top border-bottom">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} width="30" height="30" className="d-inline-block align-top pr-1" alt="" />
+          Recipease
+      </Link>
+        <button data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" className='navbar-toggler collapsed' onClick={handleToggleNav} type="button">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={(navCollapsed ? 'collapse' : '') + ' navbar-collapse'}>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={handleToggleNav}>Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/recipes" onClick={handleToggleNav}>Recipes</Link>
+            </li>
             {isLoggedin ? (
-            <>
               <li className="nav-item">
-                <Link className="nav-link" to="/"><button className="btn btn-outline-primary" type="submit" onClick={this.handleLogout}>Logout</button></Link>
+                <Link className="nav-link" to="/profile" onClick={handleToggleNav}>Profile</Link>
               </li>
-            </>
+            ) : (null)}
+          </ul>
+
+          <ul className="navbar-nav">
+            {isLoggedin ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/"><button className="btn btn-outline-primary" type="submit" onClick={handleLogout}>Logout</button></Link>
+                </li>
+              </>
             ) :
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login"><button className="btn btn-outline-primary" type="submit" onClick={this.handleToggleNav}>Login</button></Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup"><button className="btn btn-primary" type="submit" onClick={this.handleToggleNav}>Sign up</button></Link>
-              </li>
-            </>
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login"><button className="btn btn-outline-primary" type="submit" onClick={handleToggleNav}>Login</button></Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup"><button className="btn btn-primary" type="submit" onClick={handleToggleNav}>Sign up</button></Link>
+                </li>
+              </>
             }
-            </ul>
-          </div>
+          </ul>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
 export default withRouter(withAuth(Navbar));
