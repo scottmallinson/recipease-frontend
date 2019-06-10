66d40167f28e30b879cf1bc9c47839f8f1ba0929
Scott Mallinson <scott@scottmallinson.com>
2019-06-10 17:02:41 +0200

    Converted Class component to React hooks

--- src/components/FeaturedRecipeCard.js
+++ src/components/FeaturedRecipeCard.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
 const moment = require('moment');
 
-class FeaturedRecipeCard extends Component {
-  state = {
-    recipe: this.props.recipe
-  }
-  render() {
-    const { _id, name, description, photoUrl, updated_at } = this.state.recipe;
-    return (
-      <div className="card">
-        <Link to={{
-          pathname: `/recipes/${_id}`,
-          state: { selectedRecipe: this.state.recipe }
-        }}>
-          <img src={photoUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">
-              {name}
-            </h5>
-            <p className="card-text text-body">{description}</p>
-            <p className="card-text"><small className="text-muted">Last updated {moment(updated_at).fromNow()}</small></p>
-          </div>
-        </Link>
-      </div>
-    )
-  }
+function FeaturedRecipeCard(props) {
+  const [recipe] = useState(props.recipe);
+  const { _id, name, description, photoUrl, updated_at } = recipe;
+  return (
+    <div className="card">
+      <Link to={{
+        pathname: `/recipes/${_id}`,
+        state: { selectedRecipe: recipe }
+      }}>
+        <img src={photoUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">
+            {name}
+          </h5>
+          <p className="card-text text-body">{description}</p>
+          <p className="card-text"><small className="text-muted">Last updated {moment(updated_at).fromNow()}</small></p>
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export default FeaturedRecipeCard;
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
-class RecipeCard extends Component {
-  state = {
-    recipe: this.props.recipe
-  }
-  render() {
-    const { _id, name, description, photoUrl } = this.state.recipe;
-    return (
-      <div className="card mb-3">
-        <Link to={{
-          pathname: `/recipes/${_id}`,
-          state: { selectedRecipe: this.state.recipe }
-        }}>
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <img src={photoUrl} className="card-img" alt="..." />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">
-                  {name}
-                </h5>
-                <p className="card-text text-body">{description}</p>
-              </div>
+function RecipeCard(props) {
+  const [recipe] = useState(props.recipe)
+  const { _id, name, description, photoUrl } = recipe;
+  return (
+    <div className="card mb-3">
+      <Link to={{
+        pathname: `/recipes/${_id}`,
+        state: { selectedRecipe: recipe }
+      }}>
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src={photoUrl} className="card-img" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">
+                {name}
+              </h5>
+              <p className="card-text text-body">{description}</p>
             </div>
           </div>
-        </Link>
-      </div>
-    )
-  }
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export default RecipeCard;
